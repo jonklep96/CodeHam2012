1953e7228e4625d90b32a0122793838f32bbdd82
jonklep96 <jonklepp@gmail.com>
2016-02-06 11:44:25 -0500

    v0.03.1j
    Very Broken Window - Updated code

--- color.py
+++ color.py
@@ -1,3 +1,17 @@
+# Monochromatic Colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+GRAY = (33, 33, 33)
 HOT_PINK = (255, 30, 180)
+
+# Themed Red Colors
+BOT = (151, 8, 8)
+
+# Themed Blue Colors
+BLUE = (8, 40, 151)
+
+# Themed Green Colors
+BYT = (5, 90, 90)
+
+# Themed Yellow Colors
+YELLOW = (151, 103, 8)
--- window.py
+++ window.py
@@ -1,68 +1,112 @@
-import os
 import sys
 import pygame
-from pygame.locals import *
 import color
+
+pygame.init()
 
 
 class Window:
 
     def __init__(self):
 
-        pygame.init()
-        self.WIDTH = 720
-        self.HEIGHT = 600
-        self.display_size = [self.WIDTH, self.HEIGHT]
-        self.cell_width = int(self.WIDTH / 12)
-        self.cell_height = int(self.HEIGHT / 12)
-        self.display_label = 'Code Ham Game'
-        self.display = pygame.display.set_mode(self.display_size)
-        pygame.display.set_caption(self.display_label)
+        self.width = 540
+        self.height = 540
+        self.CELL_HOR = 15
+        self.CELL_VER = 15
+        self.OUTER_CELLS = 2
+        self.cell_width = int(self.width / self.CELL_HOR)
+        self.cell_height = int(self.height / self.CELL_VER)
+        self.screen_label = 'Code Ham Game'
+        self.screen = pygame.display.set_mode([self.width, self.height], pygame.RESIZABLE)
+        self.clear_window()
+        pygame.display.set_caption(self.screen_label)
+        self.grid_surface = pygame.Surface((self.width, self.height))
 
         # List to store cell coordinates
         self.grid = []
+        self.sel_cells = [0]*4
         self.last_loc = [0, 0]
         # Constant of the cell border width
         self.GRID_CELL_WIDTH = 2
-        self.draw_grid()
+        self.draw_grid(True)
 
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     pos = pygame.mouse.get_pos()
-                    print(pos)
+                    print(pos)  # debug mouse position
                     self.check_click(pos)
-                if event.type == pygame.QUIT:
+                elif event.type == pygame.VIDEORESIZE:
+                    self.init_sizes(event)
+                    sub_width = self.grid_surface.get_width()
+                    sub_height = self.grid_surface.get_height()
+                    sub_x = int(self.width - sub_width) / 2
+                    sub_y = int(self.height - sub_height) / 2
+                    self.screen.blit(self.grid_surface, (sub_x, sub_y))
+                    self.clear_window()
+                    self.draw_grid(False)
+                    pygame.display.update()
+                    print('Screen Change')
+                elif event.type == pygame.QUIT:
                     sys.exit()
             pygame.display.update()
 
-    # draws the grid according to the screen size
-    def draw_grid(self):
+    def init_sizes(self, event):
 
-        for x in range(0, self.WIDTH, self.cell_width):
-            for y in range(0, self.HEIGHT, self.cell_height):
+        size = [event.w, event.h]
+        self.width = size[0]
+        self.height = size[1]
+
+    # draws the grid according to the screen size
+    # during the first_run, draw_grid will add the
+    # rects to a list
+    def draw_grid(self, first_run):
+
+        for x in range(0, self.width, self.cell_width):
+            for y in range(0, self.height, self.cell_height):
                 rect = pygame.Rect(x, y, self.cell_width, self.cell_height)
-                self.grid.append([[x, x + self.cell_width], [y, y + self.cell_height]])
-                pygame.draw.rect(self.display, color.WHITE, rect, self.GRID_CELL_WIDTH)
+                if first_run:
+                    self.grid.append(rect)
+                pygame.draw.rect(self.grid_surface, color.WHITE, rect, self.GRID_CELL_WIDTH)
 
     # checks for which grid you clicked in
     def check_click(self, pos):
 
-        # Values to store clicking and drawing
-        padding_adjustment = self.GRID_CELL_WIDTH + 3
-        rect_display = pygame.Rect(0, 0, self.WIDTH, self.HEIGHT)
-
-        coordinate = 0
+        grid_index = 0
         for item in self.grid:
-            coordinate += 1
-            if pos[0] > item[0][0] + padding_adjustment:
-                if pos[0] < item[0][1] - padding_adjustment:
-                    if pos[1] > item[1][0] + padding_adjustment:
-                        if pos[1] < item[1][1] - padding_adjustment:
-                            print(coordinate) #print place debug
-                            new_rect = pygame.Rect(item[0][0], item[1][0], self.cell_width, self.cell_height)
-                            pygame.draw.rect(self.display, color.BLACK, rect_display)
-                            self.draw_grid()
-                            pygame.draw.rect(self.display, color.HOT_PINK, new_rect, self.GRID_CELL_WIDTH + 2)
-                            self.last_loc = [item[0][0], item[1][0]]
-                            break
+            if self.rect_contain(item, pos):
+                print(grid_index)  # print place debug
+                new_rect = pygame.Rect(item.x, item.y, self.cell_width, self.cell_height)
+                self.clear_window()
+                self.draw_grid(False)
+                # Selectable grid choices
+                self.sel_cells[0] = self.grid[grid_index - 1]
+                self.sel_cells[1] = self.grid[grid_index + self.CELL_HOR]
+                self.sel_cells[2] = self.grid[grid_index + 1]
+                self.sel_cells[3] = self.grid[grid_index - self.CELL_HOR]
+                # Draw the selectable locations
+                pygame.draw.rect(self.grid_surface, color.BLUE, self.sel_cells[0], self.GRID_CELL_WIDTH + 1)
+                pygame.draw.rect(self.grid_surface, color.BLUE, self.sel_cells[1], self.GRID_CELL_WIDTH + 1)
+                pygame.draw.rect(self.grid_surface, color.BLUE, self.sel_cells[2], self.GRID_CELL_WIDTH + 1)
+                pygame.draw.rect(self.grid_surface, color.BLUE, self.sel_cells[3], self.GRID_CELL_WIDTH + 1)
+                pygame.draw.rect(self.grid_surface, color.YELLOW, new_rect, self.GRID_CELL_WIDTH + 2)
+                self.last_loc = item
+            grid_index += 1
+
+    # Check to see if the position was inside the specified cell
+    def rect_contain(self, rect, pos):
+
+        # Values to store clicking and drawing
+        padding_adjustment = self.GRID_CELL_WIDTH + 1
+
+        if pos[0] > rect.x + padding_adjustment:
+            if pos[0] < rect.x + rect.width - padding_adjustment:
+                if pos[1] > rect.y + padding_adjustment:
+                    if pos[1] < rect.y + rect.height - padding_adjustment:
+                        return True
+        else:
+            return False
+
+    def clear_window(self):
+
+        pygame.draw.rect(self.screen, color.GRAY, (0, 0, self.width, self.height))
