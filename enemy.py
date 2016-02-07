422f2d35332141e6399d5d2684c70f5d342ea105
jonklep96 <jonklepp@gmail.com>
2016-02-07 02:50:22 -0500

    Still Building AI

--- enemy.py
+++ enemy.py
@@ -12,3 +12,28 @@ class Enemy(unit.Unit):
 
         self.money = int(random.random() * 5 + 1)
         self.atk = 2
+
+    def get_move_dir(self, byt_rect):
+
+        if self.rect.x < byt_rect.x:
+            if self.rect.y < byt_rect.y:
+                return 2
+            elif self.rect.y > byt_rect.y:
+                return 0
+        elif self.rect.y > byt_rect.y:
+            if self.rect.x < byt_rect.x:
+                return 3
+            elif self.rect.x > byt_rect.x:
+                return 1
+        elif self.rect.x > byt_rect.x:
+            return 1
+        elif self.rect.y < byt_rect.y:
+            return 2
+        else:
+            return 0
+
+    # Is called when the bot is moved
+    def move(self, _rect, _loc):
+
+        self.rect = _rect
+        self.loc = _loc
--- unit.py
+++ unit.py
@@ -1,27 +1,28 @@
 import pygame
 
+
 class Unit(pygame.sprite.Sprite):
 
-    #Constructor
+    # Constructor
     def __init__(self, _rect, img, _name):
         self.name = _name
 
-        #Calling the parent constructor, pygame.sprite.Sprite
+        # Calling the parent constructor, pygame.sprite.Sprite
         pygame.sprite.Sprite.__init__(self)
 
-        #This is the rectangle where the unit resides
+        # This is the rectangle where the unit resides
         x = _rect.x
         y = _rect.y
         width = 32
         height = 32
         self.rect = pygame.Rect(x, y, width, height)
 
-        #Loading the sprite
+        # Loading the sprite
         self.image = pygame.image.load('assets/' + img + '.png')
 
-        #Instance Variables for Byts and Bots:
+        # Instance Variables for Byts and Bots:
         self.health = 10
         self.money = 0
         self.atk = 1
         self.speed = 1
-        
+        self.loc = -1
